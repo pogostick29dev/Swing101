5ba07fa21b6843537d7904c92b92f6dea3fe4526
nrubin29 <nrubin29@gmail.com>
2014-05-08 21:35:18 -0400

    Code from episode 2.

--- src/me/pogostick29dev/swing101/Swing101.java
+++ src/me/pogostick29dev/swing101/Swing101.java
@@ -1,19 +1,29 @@
 package me.pogostick29dev.swing101;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
 public class Swing101 {
 	
 	public Swing101() {
 		JFrame frame = new JFrame("Swing 101");
+		
+		JLabel label = new JLabel("Hello, world!");
+		label.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+		label.setForeground(Color.WHITE);
+		label.setAlignmentX(Component.CENTER_ALIGNMENT);
+		frame.add(label);
+		
 		frame.setSize(640, 480);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setBackground(Color.CYAN);
+		frame.getContentPane().setBackground(Color.CYAN);
 		frame.setVisible(true);
 	}
 
